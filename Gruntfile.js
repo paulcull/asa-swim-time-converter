275bf54ee3752ca166a41f58798112e062d9c6e5
Paul Cullender <paul@pkhome.co.uk>
2017-07-31 06:49:03 +0100

    remove lint & add doc

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,154 +1,167 @@
 module.exports = function(grunt) {
 
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    jshint: {
-      beforeconcat: ['Gruntfile.js', 'src/**/*.js', 'src/**/*.json', 'test/**/*.js'],
-      afterconcat: ['build/<%= pkg.name %>.js']
-    },
-    watch: {
-      files: ['<%= jshint.files %>'],
-      tasks: ['jshint','mochaTest']
-    },
-    clean: {
-      build: {
-        src: ['build']
-      },
-      dist: {
-        src: ['build','dist/**/*']
-      },
-      coverage: {
-        src: ['coverage/**']
-      }
-    },
-    concat: {
-      options: {
-        // define a string to put between each file in the concatenated output
-        separator: ';'
-      },
-      dist: {
-        // the files to concatenate (exclue example)
-        src: ['src/**/*.js','!src/conversionTables/*.js','!src/example.js'],
-        // the location of the resulting JS file
-        dest: 'build/<%= pkg.name %>.js'
-      }
-    },
-    mochaTest: {
-      test: {
-        options: {
-          reporter: 'spec',
-          captureFile: 'coverage/results.txt', // Optionally capture the reporter output to a file
-          quiet: false, // Optionally suppress output to standard out (defaults to false)
-          clearRequireCache: false, // Optionally clear the require cache before running tests (defaults to false)
-          noFail: false // Optionally set to not fail on failed tests (will still fail on other errors)
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        jshint: {
+            beforeconcat: ['Gruntfile.js', 'src/**/*.js', 'src/**/*.json', 'test/**/*.js'],
+            afterconcat: ['build/<%= pkg.name %>.js']
         },
-        src: ['test/**/*.js']
-      }
-    },
-    mocha_istanbul: {
-      coverage: {
-          src: ['test/**/*.js'],
-           // a folder works nicely
-           options: {
-               coverage:true, // this will make the grunt.event.on('coverage') event listener to be triggered
-               check: {
-                   lines: 75,
-                   statements: 75
-               },
-               root: './src', // define where the cover task should consider the root of libraries that are covered by tests
-               reportFormats: ['lcov']
-           }
-      // },
-      // coveralls: {
-      //     src: ['test'], // multiple folders also works
-      //     options: {
-      //         coverage:true, // this will make the grunt.event.on('coverage') event listener to be triggered
-      //         check: {
-      //             lines: 75,
-      //             statements: 75
-      //         },
-      //         root: './src', // define where the cover task should consider the root of libraries that are covered by tests
-      //         reportFormats: ['lcov']
-      //     }
-      }
-    },
-    istanbul_check_coverage: {
-      default: {
-        options: {
-          coverageFolder: 'coverage', // will check both coverage folders and merge the coverage results
-          check: {
-            lines: 80,
-            statements: 80
-          }
+        watch: {
+            files: ['<%= jshint.files %>'],
+            tasks: ['jshint', 'mochaTest']
+        },
+        clean: {
+            build: {
+                src: ['build']
+            },
+            dist: {
+                src: ['build', 'dist/**/*']
+            },
+            coverage: {
+                src: ['coverage/**']
+            }
+        },
+        concat: {
+            options: {
+                // define a string to put between each file in the concatenated output
+                separator: ';'
+            },
+            dist: {
+                // the files to concatenate (exclue example)
+                src: ['src/**/*.js', '!src/conversionTables/*.js', '!src/example.js'],
+                // the location of the resulting JS file
+                dest: 'build/<%= pkg.name %>.js'
+            }
+        },
+        mochaTest: {
+            test: {
+                options: {
+                    reporter: 'spec',
+                    captureFile: 'coverage/results.txt', // Optionally capture the reporter output to a file
+                    quiet: false, // Optionally suppress output to standard out (defaults to false)
+                    clearRequireCache: false, // Optionally clear the require cache before running tests (defaults to false)
+                    noFail: false // Optionally set to not fail on failed tests (will still fail on other errors)
+                },
+                src: ['test/**/*.js']
+            }
+        },
+        mocha_istanbul: {
+            coverage: {
+                src: ['test/**/*.js'],
+                // a folder works nicely
+                options: {
+                    coverage: true, // this will make the grunt.event.on('coverage') event listener to be triggered
+                    check: {
+                        lines: 75,
+                        statements: 75
+                    },
+                    root: './src', // define where the cover task should consider the root of libraries that are covered by tests
+                    reportFormats: ['lcov']
+                }
+                // },
+                // coveralls: {
+                //     src: ['test'], // multiple folders also works
+                //     options: {
+                //         coverage:true, // this will make the grunt.event.on('coverage') event listener to be triggered
+                //         check: {
+                //             lines: 75,
+                //             statements: 75
+                //         },
+                //         root: './src', // define where the cover task should consider the root of libraries that are covered by tests
+                //         reportFormats: ['lcov']
+                //     }
+            }
+        },
+        istanbul_check_coverage: {
+            default: {
+                options: {
+                    coverageFolder: 'coverage', // will check both coverage folders and merge the coverage results
+                    check: {
+                        lines: 80,
+                        statements: 80
+                    }
+                }
+            }
+        },
+        coveralls: {
+            // Options relevant to all targets
+            options: {
+                // When true, grunt-coveralls will only print a warning rather than
+                // an error, to prevent CI builds from failing unnecessarily (e.g. if
+                // coveralls.io is down). Optional, defaults to false.
+                force: false
+            },
+            your_target: {
+                // LCOV coverage file (can be string, glob or array)
+                src: 'coverage/*.info',
+                options: {
+                    // Any options for just this target
+                }
+            },
+        },
+        uglify: {
+            options: {
+                banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy - mm - dd ") %> */\n'
+            },
+            build: {
+                src: 'build/<%= pkg.name %>.js',
+                dest: 'dist/<%= pkg.name %>.min.js'
+            }
+        },
+        copy: {
+            conversionTables: {
+                expand: true,
+                cwd: 'src/conversionTables',
+                src: '*.js',
+                dest: 'dist/conversionTables',
+            },
+        },
+        release: {
+            options: {
+                bump: 'false', //only from version is changed
+            }
+        },
+        documentation: {
+            default: {
+                files: [{
+                    'expand': true,
+                    'cwd': 'src',
+                    'src': ['**/*.js']
+                }],
+                options: {
+                    destination: 'docs'
+                }
+            }
         }
-      }
-    },
-    coveralls: {
-      // Options relevant to all targets
-      options: {
-        // When true, grunt-coveralls will only print a warning rather than
-        // an error, to prevent CI builds from failing unnecessarily (e.g. if
-        // coveralls.io is down). Optional, defaults to false.
-        force: false
-      },
-      your_target: {
-        // LCOV coverage file (can be string, glob or array)
-        src: 'coverage/*.info',
-        options: {
-          // Any options for just this target
-        }
-      },
-    },
-    uglify: {
-      options: {
-        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
-      },
-      build: {
-        src: 'build/<%= pkg.name %>.js',
-        dest: 'dist/<%= pkg.name %>.min.js'
-      }
-    },
-    copy: {
-      conversionTables: {
-        expand: true,
-        cwd: 'src/conversionTables',
-        src: '*.js',
-        dest: 'dist/conversionTables',
-      },
-    },
-    release: {
-      options: {
-        bump: 'false', //only from version is changed
-      }
-    }
-  });
+    });
 
-  grunt.event.on('coverage', function(lcovFileContents, done){
-      // Check below on the section "The coverage event"
-      done();
-  });
+    grunt.event.on('coverage', function(lcovFileContents, done) {
+        // Check below on the section 'The coverage event'
+        done();
+    });
 
-  //load the dependencies
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-concat');
-  grunt.loadNpmTasks('grunt-mocha-test');
-  grunt.loadNpmTasks('grunt-mocha-istanbul');
-  grunt.loadNpmTasks('grunt-coveralls');
-  grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-release');
-  grunt.loadNpmTasks('grunt-contrib-copy');
+    //load the dependencies
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-mocha-test');
+    grunt.loadNpmTasks('grunt-mocha-istanbul');
+    grunt.loadNpmTasks('grunt-coveralls');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-release');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-documentation');
 
-  //set the tasks / taskmaps
-  grunt.registerTask('reportCoveralls', ['clean:coverage','mocha_istanbul:coverage','coveralls']);
-  grunt.registerTask('coverage', ['clean:coverage','mocha_istanbul:coverage']);
-  grunt.registerTask('ci-build', ['clean:coverage','clean:dist','jshint:beforeconcat','mochaTest','reportCoveralls','concat','jshint:afterconcat','uglify','copy:conversionTables']);
-  grunt.registerTask('build', ['clean:dist','jshint:beforeconcat','mochaTest','concat','jshint:afterconcat','uglify','copy:conversionTables']);
-  grunt.registerTask('test', ['clean:coverage','jshint','mochaTest','coverage']);
-  grunt.registerTask('watch', ['watch']);
-  grunt.registerTask('releaseToNPM',['release']);
+    //set the tasks / taskmaps
+    grunt.registerTask('reportCoveralls', ['clean:coverage', 'mocha_istanbul:coverage', 'coveralls']);
+    grunt.registerTask('coverage', ['clean:coverage', 'mocha_istanbul:coverage']);
+    grunt.registerTask('ci-build', ['clean:coverage', 'clean:dist', 'jshint:beforeconcat', 'mochaTest', 'documentation', 'reportCoveralls', 'concat', 'jshint:afterconcat', 'uglify', 'copy:conversionTables']);
+    grunt.registerTask('build', ['clean:dist', 'jshint:beforeconcat', 'mochaTest', 'documentation', 'concat', 'jshint:afterconcat', 'uglify', 'copy:conversionTables']);
+    grunt.registerTask('test', ['clean:coverage', 'jshint', 'mochaTest', 'coverage']);
+    grunt.registerTask('watch', ['watch']);
+    grunt.registerTask('releaseToNPM', ['release']);
 
-  grunt.registerTask('default', ['build']);
+    grunt.registerTask('default', ['build']);
 
 };
--- src/index.js
+++ src/index.js
@@ -1,155 +1,159 @@
-var strokes = require('./conversionTables/strokeTable');
-var pools = require('./conversionTables/poolTable');
+(function() {
+    'use strict';
 
-var timeFormatRegex = new RegExp("^([0-9]|0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$");
-var numPlaces = 1;
+    var strokes = require('./conversionTables/strokeTable');
+    var pools = require('./conversionTables/poolTable');
 
-var isDebugMode = false;
+    var numPlaces = 1;
+    var isDebugMode = false;
 
 
-//debug log support
-console.debug = function( /* ...args */ ) {
-    if (isDebugMode) {
-        var vargs = Array.prototype.slice.call(arguments);
-        console.log.apply(this, vargs);
+    //debug log support
+    console.debug = function( /* ...args */ ) {
+        if (isDebugMode) {
+            var vargs = Array.prototype.slice.call(arguments);
+            console.log.apply(this, vargs);
+        }
+    };
+
+
+    // Exposed Function
+
+
+
+    /**
+     *  getTime function to convert Swim times based on ASA table
+     * getTime(convertFrom, convertTo, stroke, timing)
+     *
+     * based on formula documented
+     * https://www.swimmingresults.org/downloads/equivalent-time-share/algorithm.php
+     *
+     * inputs
+     * @param {string} stroke = stroke as per the list
+     * @param {string} covertFrom = poolLength
+     * @param {string} convertTo = poolLength
+     * @param {time}   timing = time to convert from
+     *
+     * returns
+     * @return {time}  time per target
+     *
+     */
+
+    function getTime(stroke, convertFrom, convertTo, timing) {
+        return new Promise(function(resolve, reject) {
+            console.debug('converting ' + stroke + ' @ ' + timing + ' from ' + convertFrom + ' to ' + convertTo);
+
+            var poolTable = {};
+            var strokeTable = {};
+            var timingInSeconds = 0;
+
+            //check stroke is a known one
+            if (!strokes[stroke]) {
+                reject('ERR: Unrecognised Stroke');
+            } else {
+                strokeTable = strokes[stroke];
+                //console.debug('Read stroke table: ', strokeTable);
+            }
+
+            //check that time doesn't contain any odd stuff
+            if (!timing.match(/[0-9,:,.]/i)) {
+                reject('ERR: Not a Valid Time 1');
+            }
+            if ((timing.match(/:/g) || []).length > 1) {
+                reject('ERR: Not a Valid Time 2');
+            }
+            if ((timing.match(/\./g) || []).length > 1) {
+                reject('ERR: Not a Valid Time 3');
+            }
+            //handle times - convert to seconds, if not already
+            try {
+                //check if minutes separator exists
+                if (timing.indexOf(':') > 0) {
+                    var _parts = timing.split(':');
+                    //assumes no times in hours
+                    timingInSeconds = (parseFloat(_parts[0]) * 60) + (parseFloat(_parts[1]));
+                } else {
+                    timingInSeconds = parseFloat(timing);
+                }
+            } catch (err) {
+                reject('ERR: Incorrect Time Conversion Error: ' + err);
+            }
+
+            //check fromPoolLength is a known one
+            if (!pools[convertFrom]) {
+                reject('ERR: Unrecognised Pool Length : From');
+            } else {
+                poolTable = pools[convertFrom];
+                //console.debug('Read pool table: ', poolTable);
+            }
+            if (!pools[convertTo]) {
+                reject('ERR: Unrecognised Pool Length : To');
+            }
+
+            // start compiling the inputs to the calc
+            try {
+
+                var PoolMeasure = poolTable.poolMeasureCase;
+                console.debug(' - PoolMeasure     :', PoolMeasure);
+                var NumTurnPH = poolTable.numTurnPerHundred;
+                console.debug(' - NumTurnPH       :', NumTurnPH);
+                var Distance = strokeTable.distance;
+                console.debug(' - Distance        :', Distance);
+                var D1 = Distance;
+                console.debug(' - timingInSeconds :', timingInSeconds);
+                //special case for 1500 in short pools
+                if (Distance === 1500 && poolTable.poolMeasureCase === 3) {
+                    D1 = 1650;
+                }
+                console.debug(' - D1             :', D1);
+                var NumbTurnFactor = Distance / 100 * (D1 / 100) * (NumTurnPH - 1);
+                console.debug(' - NumbTurnFactor :', NumbTurnFactor);
+                var TurnFactor = strokeTable.TurnFactor;
+                console.debug(' - TurnFactor     :', TurnFactor);
+
+                // The first Step is to get the value for a 50m form
+                // TimeTo50 = (swtime + Sqr(swtime * swtime + 4 * PoolMeasure * TurnFactor * NumbTurnFactor)) / (2 * PoolMeasure)
+                var T50 = 0;
+                if (convertFrom !== '50m') {
+                    T50 = (timingInSeconds +
+                        (Math.sqrt(timingInSeconds * timingInSeconds + 4 * PoolMeasure * TurnFactor * NumbTurnFactor))
+                    ) / (2 * PoolMeasure);
+                } else {
+                    T50 = timingInSeconds;
+                }
+                console.debug(' - T50            :', T50);
+
+                //The second step – 50m pool length to desired pool length – is expressed as:
+                //T50 * PoolMeasure – TurnVal * (NumTurnPH – 1)
+                // Where:
+                //
+                // T50 = Time in a 50m pool
+                // PoolMeasure = Event distance factor
+                // TurnVal = Time per turn – The time in seconds attributed to execution of a	single turn
+                // NumTurnPH = Number of turns per 100 in the required length pool.
+                // The Time per turn is determined by the relationship:
+                // TurnVal = TurnFactor / T50
+                // Where TurnFactor is provided by the LookUp table
+
+                var convertTime = 0;
+
+                var TurnVal = TurnFactor / T50;
+                console.debug(' - TurnVal        :', TurnVal);
+                if (convertTo !== '50m') {
+                    convertTime = T50 * 1 - TurnVal * 2;
+                } else {
+                    convertTime = T50;
+                }
+
+                console.debug(' - Result Time    :', convertTime);
+                resolve(convertTime.toFixed(numPlaces).toString());
+
+            } catch (err) {
+                reject('ERR: Conversion Error: ' + err);
+            }
+
+        });
     }
-};
 
-//based on formula documented
-// https://www.swimmingresults.org/downloads/equivalent-time-share/algorithm.php
-
-
-// Exposed Function
-
-/*
-  getTime function to convert Swim times based on ASA table
-  getTime(convertFrom, convertTo, stroke, timing)
-
-  inputs
-  @stroke = stroke as per the list
-  @covertFrom = poolLength
-  @convertTo = poolLength
-  @timing = time to convert from
-
-  returns
-  time per target
-
-  */
-
-function getTime(stroke, convertFrom, convertTo, timing) {
-    return new Promise(function(resolve, reject) {
-        console.debug("converting " + stroke + " @ " + timing + " from " + convertFrom + " to " + convertTo);
-
-        var poolTable = {};
-        var strokeTable = {};
-        var timingInSeconds = 0;
-
-        //check stroke is a known one
-        if (!strokes[stroke]) {
-            reject("ERR: Unrecognised Stroke");
-        } else {
-            strokeTable = strokes[stroke];
-            //console.debug('Read stroke table: ', strokeTable);
-        }
-
-        //check that time doesn't contain any odd stuff
-        if (!timing.match(/[0-9,:,.]/i)) {
-            reject("ERR: Not a Valid Time 1");
-        }
-        if ((timing.match(/:/g) || []).length > 1) {
-            reject("ERR: Not a Valid Time 2");
-        }
-        if ((timing.match(/\./g) || []).length > 1) {
-            reject("ERR: Not a Valid Time 3");
-        }
-        //handle times - convert to seconds, if not already
-        try {
-            //check if minutes separator exists
-            if (timing.indexOf(':') > 0) {
-                var _parts = timing.split(":");
-                //assumes no times in hours
-                timingInSeconds = (parseFloat(_parts[0]) * 60) + (parseFloat(_parts[1]));
-            } else {
-                timingInSeconds = parseFloat(timing);
-            }
-        } catch (err) {
-            reject("ERR: Incorrect Time Conversion Error: " + err);
-        }
-
-        //check fromPoolLength is a known one
-        if (!pools[convertFrom]) {
-            reject("ERR: Unrecognised Pool Length : From");
-        } else {
-            poolTable = pools[convertFrom];
-            //console.debug('Read pool table: ', poolTable);
-        }
-        if (!pools[convertTo]) {
-            reject("ERR: Unrecognised Pool Length : To");
-        }
-
-        // start compiling the inputs to the calc
-        try {
-
-            var PoolMeasure = poolTable.poolMeasureCase;
-            console.debug(" - PoolMeasure     :", PoolMeasure);
-            var NumTurnPH = poolTable.numTurnPerHundred;
-            console.debug(" - NumTurnPH       :", NumTurnPH);
-            var Distance = strokeTable.distance;
-            console.debug(" - Distance        :", Distance);
-            var D1 = Distance;
-            console.debug(" - timingInSeconds :", timingInSeconds);
-            //special case for 1500 in short pools
-            if (Distance === 1500 && poolTable.poolMeasureCase === 3) {
-                D1 = 1650;
-            }
-            console.debug(" - D1             :", D1);
-            var NumbTurnFactor = Distance / 100 * (D1 / 100) * (NumTurnPH - 1);
-            console.debug(" - NumbTurnFactor :", NumbTurnFactor);
-            var TurnFactor = strokeTable.TurnFactor;
-            console.debug(" - TurnFactor     :", TurnFactor);
-
-            // The first Step is to get the value for a 50m form
-            // TimeTo50 = (swtime + Sqr(swtime * swtime + 4 * PoolMeasure * TurnFactor * NumbTurnFactor)) / (2 * PoolMeasure)
-            var T50 = 0;
-            if (convertFrom != "50m") {
-                T50 = (timingInSeconds +
-                    (Math.sqrt(timingInSeconds * timingInSeconds + 4 * PoolMeasure * TurnFactor * NumbTurnFactor))
-                ) / (2 * PoolMeasure);
-            } else {
-                T50 = timingInSeconds;
-            }
-            console.debug(" - T50            :", T50);
-
-            //The second step – 50m pool length to desired pool length – is expressed as:
-            //T50 * PoolMeasure – TurnVal * (NumTurnPH – 1)
-            // Where:
-            //
-            // T50 = Time in a 50m pool
-            // PoolMeasure = Event distance factor
-            // TurnVal = Time per turn – The time in seconds attributed to execution of a	single turn
-            // NumTurnPH = Number of turns per 100 in the required length pool.
-            // The Time per turn is determined by the relationship:
-            // TurnVal = TurnFactor / T50
-            // Where TurnFactor is provided by the LookUp table
-
-            var convertTime = 0;
-
-            var TurnVal = TurnFactor / T50;
-            console.debug(" - TurnVal        :", TurnVal);
-            if (convertTo != "50m") {
-                convertTime = T50 * 1 - TurnVal * 2;
-            } else {
-                convertTime = T50;
-            }
-
-            console.debug(" - Result Time    :", convertTime);
-            resolve(convertTime.toFixed(numPlaces).toString());
-
-        } catch (err) {
-            reject("ERR: Conversion Error: " + err);
-        }
-
-    });
-}
-
-module.exports = getTime;
+    module.exports = getTime;
+}());
